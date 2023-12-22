10b06852441b7e34d866b2530c8c12c9736e13fe
DarionBlake <121829965+DarionBlake@users.noreply.github.com>
2023-12-21 18:24:30 -0600

    Create main.c

---
+++ main.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+/**
+ * main - driver func for main monty
+ * @argc: arg count
+ * @argv: arg vector
+ * Return: 0
+*/
+int main(int argc, char **argv)
+{
+	stack_t *stack;
+
+	stack = NULL;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	read_file(argv[1], &stack);
+	free_dlistint(stack);
+	return (0);
+}
